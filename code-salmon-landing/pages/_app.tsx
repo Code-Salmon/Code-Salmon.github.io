2c1e456e88fac87f962c4a953ab3af64b51bfbcf
Everett Merrill <everettwarrenmerrill@gmail.com>
2025-08-14 11:20:09 -0700

    static

--- code-salmon-landing/pages/_app.tsx
+++ code-salmon-landing/pages/_app.tsx
@@ -1,18 +1,40 @@
 // pages/_app.tsx
+import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
-<div
-  className="relative min-h-screen bg-scroll bg-cover bg-top bg-center text-[#E5E5E5]"
-  style={{ backgroundImage: "url('/images/hero-bbg.jpg')", 
-    backgroundSize: "120%"
-   }}
->
-  <div className="absolute inset-0 bg-black bg-opacity-50 z-0" />
-  <div className="relative z-10">
-    <Component {...pageProps} />
+    <div
+    className="relative min-h-screen bg-scroll bg-cover bg-top bg-center text-[#E5E5E5]"
+    style={{ backgroundImage: "url('/images/hero-bbg.jpg')", 
+      backgroundSize: "120%"
+     }}
+  >
+    <div className="absolute inset-0 bg-black bg-opacity-50 z-0" />
+    <div className="relative z-10">
+      <Component {...pageProps} />
+    </div>
   </div>
-</div>
   );
 }
+
+
+
+// // pages/_app.tsx
+// import '../styles/globals.css';
+
+// export default function App({ Component, pageProps }) {
+//   return (
+// <div
+//   className="relative min-h-screen bg-scroll bg-cover bg-top bg-center text-[#E5E5E5]"
+//   style={{ backgroundImage: "url('/images/hero-bbg.jpg')", 
+//     backgroundSize: "120%"
+//    }}
+// >
+//   <div className="absolute inset-0 bg-black bg-opacity-50 z-0" />
+//   <div className="relative z-10">
+//     <Component {...pageProps} />
+//   </div>
+// </div>
+//   );
+// }
