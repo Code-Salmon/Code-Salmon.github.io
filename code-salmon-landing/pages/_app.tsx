203547e7ee0e1cb0f6dd820517ab22924b4dcfd2
Jamie Tait <taitjamie@outlook.com>
2025-08-18 11:25:17 -0700

    out folder

--- code-salmon-landing/pages/_app.tsx
+++ code-salmon-landing/pages/_app.tsx
@@ -1,7 +1,6 @@
 // pages/_app.tsx
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
-import { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
 
